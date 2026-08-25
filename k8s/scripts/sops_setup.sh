117d2289a9e83361b594e3f9b8a0a3aa528bb275
ozneroL541 <l.21radice@gmail.com>
2026-08-25 14:02:58 +0200

    refactor: update sops configuration and script permissions

--- k8s/scripts/sops_setup.sh
+++ k8s/scripts/sops_setup.sh
@@ -18,5 +18,23 @@ if [ ! -f "${AGE_KEY_FILE}" ]; then
   # Set permissions to restrict access to the age keypair file
   chmod 600 "${AGE_KEY_FILE}"
 fi
-# Add the public key to .sops.yaml
-grep "public key:" "${AGE_KEY_FILE}" | awk '{print $3}' > .sops.yaml
+AGE_PUBLIC_KEY=$(age-keygen -y "${AGE_KEY_FILE}")
+# Add the public key to the sops configuration file if it doesn't already exist
+SOPS_CONFIG_FILE="production/.sops"
+if [ ! -f "${SOPS_CONFIG_FILE}" ]; then
+    # Create a new sops configuration file with the public key
+  echo "creation_rules:
+  - path_regex: .*
+    key_groups:
+      - age:
+          - ${AGE_PUBLIC_KEY}" > "${SOPS_CONFIG_FILE}"
+else
+    # Check if the public key is already in the sops configuration file
+  if ! grep -q "${AGE_PUBLIC_KEY}" "${SOPS_CONFIG_FILE}"; then
+      # Append the public key to the existing sops configuration file
+    echo "  - path_regex: .*" >> "${SOPS_CONFIG_FILE}"
+    echo "    key_groups:" >> "${SOPS_CONFIG_FILE}"
+    echo "      - age:" >> "${SOPS_CONFIG_FILE}"
+    echo "          - ${AGE_PUBLIC_KEY}" >> "${SOPS_CONFIG_FILE}"
+  fi
+fi
